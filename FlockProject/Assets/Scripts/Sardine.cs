7a9031164c875ade5b09920171ed77f0604c6f9c
Tom Lemmers <xizzex@outlook.com>
2019-02-27 16:48:01 +0100

    - Updated the project to unity 2018.3.5.1f - Cleaned folder structure and scene. - Cleaned scripts for efficiëncy.

---
+++ FlockProject/Assets/Scripts/Sardine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flocking
+{
+    /// <summary>Sardine instance which swims around in the level. It handles it's own flocking.</summary>
+    public class Sardine : MonoBehaviour
+    {
+        private SardineDestinationManager _SardineDestinationManager;
+        [SerializeField] private float _MoveSpeed = 2, _GroupRange = 2, _AvoidanceRange = 1.5f, _RotationSpeed = 1.0f;
+
+        private void Start()
+        {
+            _SardineDestinationManager = FindObjectOfType<SardineDestinationManager>();
+        }
+
+        private void Update()
+        {
+            MaintainFlock(_SardineDestinationManager.AllSardine);
+            MoveSaldineForward();
+        }
+
+        private void MaintainFlock(Sardine[] allSardines)
+        {
+            List<Sardine> flockFishes = new List<Sardine>();
+            Vector3 avoidance = Vector3.zero;
+
+            int flockSize = 0;
+            foreach (Sardine sardine in allSardines)
+            {
+                //Check if the sardine is within group distance, so it can join/form a group.
+                if (Vector3.Distance(sardine.transform.position, transform.position) >= _GroupRange) continue;
+
+                flockFishes.Add(sardine);
+                flockSize++;
+
+                //Check if the sardine come to close to each other.
+                if (Vector3.Distance(sardine.transform.position, transform.position) < _AvoidanceRange)
+                    avoidance = avoidance + (transform.position - sardine.transform.position);
+            }
+
+            if (flockSize <= 0) return;
+
+            //Calculate the flock center.
+            Vector3 flockCenter = Vector3.zero;
+            foreach (Sardine sardine in flockFishes)
+                flockCenter += sardine.transform.position;
+            flockCenter /= flockFishes.Count;
+
+            Vector3 center = flockCenter + (_SardineDestinationManager.GetFlockDestination - transform.position);
+            Vector3 direction = (center + avoidance) - transform.position;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), _RotationSpeed * Time.deltaTime);
+        }
+
+        private void MoveSaldineForward()
+        {
+            transform.Translate(Vector3.forward * _MoveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ FlockProject/Assets/Scripts/SardineDestinationManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Flocking
+{
+    /// <summary>Gives the flock a new randomly generated destination within the specified borders.</summary>
+    public class SardineDestinationManager : MonoBehaviour
+    {
+        [Header("Fish settings")]
+        [SerializeField] private int _FlockObjectCount;
+        [SerializeField] private GameObject _FlockObjectPrefab;
+
+        [Header("SpawnSpace and border for the fishes")]
+        [SerializeField] private float _LeftWall;
+        [SerializeField] private float _RightWall, _GroundWall, _UpperWall, _FrontSide, _BackSide;
+
+        private Sardine[] _AllSardine;
+        private Vector3 _FlockDestination;
+        private float _DestinationTimer;
+
+        public Sardine[] AllSardine => _AllSardine;
+        public Vector3 GetFlockDestination => _FlockDestination;
+
+        private void Awake()
+        {
+            for (int index = 0; index < _FlockObjectCount; index++)
+            {
+                Vector3 spawnPosition = spawnPosition = new Vector3(
+                    x: Random.Range(_LeftWall, _RightWall),
+                    y: Random.Range(_GroundWall, _UpperWall),
+                    z: Random.Range(_FrontSide, _BackSide));
+                Instantiate(_FlockObjectPrefab, spawnPosition, transform.rotation, transform);
+            }
+            _AllSardine = FindObjectsOfType<Sardine>();
+        }
+
+        private void Update()
+        {
+            HandleDestinationTimer();
+        }
+
+        private void HandleDestinationTimer()
+        {
+            _DestinationTimer -= Time.deltaTime;
+            if (!(_DestinationTimer <= 0)) return;
+
+            _FlockDestination = new Vector3(
+                Random.Range(_LeftWall, _RightWall),
+                Random.Range(_GroundWall, _UpperWall),
+                Random.Range(_FrontSide, _BackSide));
+
+            _DestinationTimer = Random.Range(2, 4);
+        }
+    }
+}
